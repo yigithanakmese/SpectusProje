63dc73ba3d7636c2c533b46fe908f123cf75e418
yigithanakmese <yigithan.msn@hotmail.com>
2023-05-25 19:31:10 +0300

    karşılaşrıma değişiklikleri

--- WindowsFormsApp3/Form3.Designer.cs
+++ WindowsFormsApp3/Form3.Designer.cs
@@ -32,45 +32,13 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(31)))), ((int)(((byte)(68)))));
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.comboBox1.ForeColor = System.Drawing.Color.White;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(60, 8);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(229, 28);
-            this.comboBox1.TabIndex = 4;
-            this.comboBox1.Text = "Oyuncu Seçin";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(389, 14);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 22);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "vs";
             // 
             // comboBox2
             // 
@@ -79,7 +47,7 @@
             this.comboBox2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.comboBox2.ForeColor = System.Drawing.Color.White;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(527, 8);
+            this.comboBox2.Location = new System.Drawing.Point(495, 9);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(229, 28);
             this.comboBox2.TabIndex = 4;
@@ -89,125 +57,84 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(6)))), ((int)(((byte)(31)))));
-            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.comboBox2);
-            this.panel3.Controls.Add(this.label1);
             this.panel3.Location = new System.Drawing.Point(45, 44);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(816, 48);
             this.panel3.TabIndex = 12;
-            // 
-            // chart1
-            // 
-            this.chart1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(31)))), ((int)(((byte)(67)))));
-            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
-            chartArea1.AxisX.MajorGrid.Enabled = false;
-            chartArea1.AxisX.MajorTickMark.Enabled = false;
-            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.White;
-            chartArea1.AxisX2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
-            chartArea1.AxisX2.TitleFont = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            chartArea1.AxisX2.TitleForeColor = System.Drawing.Color.White;
-            chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
-            chartArea1.AxisY.IsLabelAutoFit = false;
-            chartArea1.AxisY.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(106)))), ((int)(((byte)(147)))));
-            chartArea1.AxisY.MajorGrid.Enabled = false;
-            chartArea1.AxisY.MajorTickMark.Enabled = false;
-            chartArea1.AxisY.MinorGrid.Enabled = true;
-            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.White;
-            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
-            chartArea1.AxisY2.IsMarginVisible = false;
-            chartArea1.AxisY2.TitleFont = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            chartArea1.AxisY2.TitleForeColor = System.Drawing.Color.White;
-            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
-            chartArea1.BorderColor = System.Drawing.Color.Transparent;
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(31)))), ((int)(((byte)(68)))));
-            legend1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            legend1.ForeColor = System.Drawing.Color.White;
-            legend1.IsTextAutoFit = false;
-            legend1.Name = "Legend1";
-            legend1.TitleBackColor = System.Drawing.Color.Black;
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(308, 129);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Radar;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            series1.YValuesPerPoint = 2;
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Radar;
-            series2.Legend = "Legend1";
-            series2.Name = "Series2";
-            this.chart1.Series.Add(series1);
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(387, 351);
-            this.chart1.TabIndex = 15;
-            this.chart1.Text = "chart1";
             // 
             // chart2
             // 
             this.chart2.BackColor = System.Drawing.Color.Transparent;
             this.chart2.BorderSkin.BackColor = System.Drawing.Color.Transparent;
             this.chart2.BorderSkin.BorderColor = System.Drawing.Color.White;
-            chartArea2.AxisX.InterlacedColor = System.Drawing.Color.White;
-            chartArea2.AxisX.LabelStyle.ForeColor = System.Drawing.Color.White;
-            chartArea2.AxisX.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX.MajorTickMark.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX.MinorGrid.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX.MinorTickMark.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX.ScaleBreakStyle.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX.TitleForeColor = System.Drawing.Color.White;
-            chartArea2.AxisX2.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX2.MajorGrid.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX2.MajorTickMark.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX2.MinorGrid.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX2.MinorTickMark.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisX2.TitleForeColor = System.Drawing.Color.White;
-            chartArea2.AxisY.LabelStyle.ForeColor = System.Drawing.Color.White;
-            chartArea2.AxisY.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisY.MajorTickMark.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisY.MinorGrid.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisY.MinorTickMark.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisY.TitleForeColor = System.Drawing.Color.White;
-            chartArea2.AxisY2.LineColor = System.Drawing.Color.White;
-            chartArea2.AxisY2.TitleForeColor = System.Drawing.Color.White;
-            chartArea2.BackColor = System.Drawing.Color.Transparent;
-            chartArea2.BackImageTransparentColor = System.Drawing.Color.Transparent;
-            chartArea2.BorderColor = System.Drawing.Color.White;
-            chartArea2.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea2);
-            legend2.BackColor = System.Drawing.Color.Transparent;
-            legend2.ForeColor = System.Drawing.Color.White;
-            legend2.HeaderSeparatorColor = System.Drawing.Color.White;
-            legend2.ItemColumnSeparatorColor = System.Drawing.Color.White;
-            legend2.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column;
-            legend2.Name = "Legend1";
-            legend2.TitleBackColor = System.Drawing.Color.Transparent;
-            legend2.TitleForeColor = System.Drawing.Color.White;
-            legend2.TitleSeparatorColor = System.Drawing.Color.White;
-            this.chart2.Legends.Add(legend2);
-            this.chart2.Location = new System.Drawing.Point(261, 129);
+            chartArea1.AxisX.InterlacedColor = System.Drawing.Color.White;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.MajorTickMark.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.MinorTickMark.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.ScaleBreakStyle.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX.TitleForeColor = System.Drawing.Color.White;
+            chartArea1.AxisX2.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX2.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX2.MajorTickMark.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX2.MinorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX2.MinorTickMark.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisX2.TitleForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.MinorGrid.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.MinorTickMark.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY2.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY2.TitleForeColor = System.Drawing.Color.White;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.BackImageTransparentColor = System.Drawing.Color.Transparent;
+            chartArea1.BorderColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.ForeColor = System.Drawing.Color.White;
+            legend1.HeaderSeparatorColor = System.Drawing.Color.White;
+            legend1.ItemColumnSeparatorColor = System.Drawing.Color.White;
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column;
+            legend1.Name = "Legend1";
+            legend1.TitleBackColor = System.Drawing.Color.Transparent;
+            legend1.TitleForeColor = System.Drawing.Color.White;
+            legend1.TitleSeparatorColor = System.Drawing.Color.White;
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(62, 119);
             this.chart2.Name = "chart2";
-            series3.ChartArea = "ChartArea1";
-            series3.EmptyPointStyle.LabelForeColor = System.Drawing.Color.White;
-            series3.LabelForeColor = System.Drawing.Color.White;
-            series3.Legend = "Legend1";
-            series3.Name = "Oyuncu 1";
-            series3.SmartLabelStyle.CalloutLineColor = System.Drawing.Color.White;
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Oyuncu 2";
-            this.chart2.Series.Add(series3);
-            this.chart2.Series.Add(series4);
-            this.chart2.Size = new System.Drawing.Size(476, 405);
+            series1.ChartArea = "ChartArea1";
+            series1.EmptyPointStyle.LabelForeColor = System.Drawing.Color.White;
+            series1.LabelForeColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.Name = "Oyuncu 1";
+            series1.SmartLabelStyle.CalloutLineColor = System.Drawing.Color.White;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Oyuncu 2";
+            this.chart2.Series.Add(series1);
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(799, 405);
             this.chart2.TabIndex = 16;
             this.chart2.Text = "chart2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(13, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(383, 21);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Karşılaştırmak istediğiniz oyuncuları sırayla seçiniz.";
             // 
             // Form3
             // 
@@ -216,26 +143,21 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(31)))), ((int)(((byte)(68)))));
             this.ClientSize = new System.Drawing.Size(918, 572);
             this.Controls.Add(this.chart2);
-            this.Controls.Add(this.chart1);
             this.Controls.Add(this.panel3);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- WindowsFormsApp3/Form3.cs
+++ WindowsFormsApp3/Form3.cs
@@ -15,61 +15,131 @@ namespace WindowsFormsApp3
 {
     public partial class Form3 : Form
     {
+
         public Form3()
         {
             InitializeComponent();
-
             SqlConnection con = new SqlConnection("Data Source=ETHN-BILGISAYAR\\SQLEXPRESS;Initial Catalog=futbol;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             SqlDataAdapter da = new SqlDataAdapter("select oyuncu_adi from oyuncu", con);
-            DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
             con.Open();
-            da.Fill(dt);
-            comboBox1.DataSource = dt;
+            da.Fill(ds, "oyuncu");
+            comboBox1.DataSource = ds.Tables["oyuncu"];
             comboBox1.DisplayMember = "oyuncu_adi";
-            comboBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
-            con.Close();
-
-            SqlDataAdapter da2 = new SqlDataAdapter("select oyuncu_adi from oyuncu", con);
-            DataTable dt2 = new DataTable();
-            con.Open();
-            da2.Fill(dt2);
-            comboBox2.DataSource = dt2;
+            comboBox2.DataSource = ds.Tables["oyuncu"];
             comboBox2.DisplayMember = "oyuncu_adi";
-            comboBox2.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             con.Close();
 
-
+         /* con.Open();
+            chart2.Series.Clear();
+            foreach (DataRow row in ds.Tables["oyuncu"].Rows)
+            {
+                string oyuncuismi = row["oyuncu_adi"].ToString();
+                chart2.Series.Add(oyuncuismi);
+                chart2.Series[oyuncuismi].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+            }
+            con.Close(); */
         }
-
-
-
-
-
-
-
 
         private void Form3_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void chart1_Click(object sender, EventArgs e)
+        private void chart2_Click(object sender, EventArgs e)
         {
 
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
-        }
+           // karsilastir();
 
+        }
+       
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
+            SqlConnection con = new SqlConnection("Data Source=ETHN-BILGISAYAR\\SQLEXPRESS;Initial Catalog=futbol;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
+            // karsilastir();
+
+            if (chart2.Series.Count > 0)
+            {
+                chart2.Series.RemoveAt(0);
+            }
+
+            string selectedPlayer = comboBox2.Text;
+
+            if (!string.IsNullOrEmpty(selectedPlayer))
+            {
+
+                chart2.Series.Add(selectedPlayer);
+                chart2.Series[selectedPlayer].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+
+
+                string query = "select * from oyuncu where oyuncu_adi = @oyuncu_adi";
+                SqlCommand com = new SqlCommand(query, con);
+                com.Parameters.AddWithValue("@oyuncu_adi", selectedPlayer);
+
+                con.Open();
+                SqlDataReader r = com.ExecuteReader();
+
+                if (r.Read())
+                {
+                    int gol = Convert.ToInt32(r["oyuncu_gol"]);
+                    int mac = Convert.ToInt32(r["oyuncu_mac_sayi"]);
+                    int pas = Convert.ToInt32(r["oyuncu_pas"]);
+                    int müc = Convert.ToInt32(r["oyuncu_mucadele_kazanilan"]);
+                    int rating = Convert.ToInt32(r["oyuncu_rating"]);
+                    int asist = Convert.ToInt32(r["oyuncu_asist"]);
+                    int sari = Convert.ToInt32(r["oyuncu_sari"]);
+                    int kirmizi = Convert.ToInt32(r["oyuncu_kirmizi"]);
+
+                    chart2.Series[selectedPlayer].Points.AddXY("Rating x100", rating/100);
+                    chart2.Series[selectedPlayer].Points.AddXY("Gol", gol);
+                    chart2.Series[selectedPlayer].Points.AddXY("Asist", asist);
+                    chart2.Series[selectedPlayer].Points.AddXY("Pas", pas);
+                    chart2.Series[selectedPlayer].Points.AddXY("Sarı Kart", sari);
+                    chart2.Series[selectedPlayer].Points.AddXY("Kırmızı Kart", kirmizi);
+                }
+
+                r.Close();
+                con.Close();
+            }
         }
 
+        private void karsilastir()
+        {
+            SqlConnection con = new SqlConnection("Data Source=ETHN-BILGISAYAR\\SQLEXPRESS;Initial Catalog=futbol;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            //string secilioyuncu = comboBox1.Text;
+            string secilioyuncu2 = comboBox2.Text;
+            if (!string.IsNullOrEmpty(secilioyuncu) && !string.IsNullOrEmpty(secilioyuncu2))
+            { 
+                SqlCommand com = new SqlCommand("select * from oyuncu where oyuncu_adi in (@secilioyuncu, @secilioyuncu2)", con);
+                com.Parameters.AddWithValue("@secilioyuncu", secilioyuncu);
+                com.Parameters.AddWithValue("@secilioyuncu2", secilioyuncu2);
 
+                con.Open();
+                SqlDataReader r = com.ExecuteReader();
+                while (r.Read())
+                {
+                    string oyuncuismi = r["oyuncu_adi"].ToString();
+                    int gol = Convert.ToInt32(r["oyuncu_gol"]);
+                    int asist = Convert.ToInt32(r["oyuncu_asist"]);
+                    int sari = Convert.ToInt32(r["oyuncu_sari"]);
+                    int kirmizi = Convert.ToInt32(r["oyuncu_kirmizi"]);
+
+                    chart2.Series[oyuncuismi].Points.Clear();
+                    chart2.Series[oyuncuismi].Points.AddXY("Gol", gol);
+                    chart2.Series[oyuncuismi].Points.AddXY("Asist", asist);
+                    chart2.Series[oyuncuismi].Points.AddXY("Sarı kart", sari);
+                    chart2.Series[oyuncuismi].Points.AddXY("Kırmızı kart", kirmizi);
+                }
+                r.Close();
+                con.Close();
+            }
+        }
 
     }
 }
